239d0396e663d9806c4bd9fbc28bc43a324b3a48
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-01-26 16:18:31 +0200

    Add SingleColorPalette and Link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
+import SingleColorPalette from "./SingleColorPalette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
@@ -37,7 +38,7 @@ class App extends Component {
         />
         <Route
           path="/palette/:paletteId/:colorId"
-          render={() => <h1>SINGLE COLOR</h1>}
+          render={() => <SingleColorPalette />}
         />
       </Switch>
 
