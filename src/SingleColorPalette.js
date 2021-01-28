b95647f1006d141df6e3bdf48bebc67be5fbe9a2
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-01-28 16:44:16 +0200

    Adding Navbar and Footer

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,12 +1,18 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { Component } from "react";
+import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
 import ColorBox from "./ColorBox";
 
 class SingleColorPalette extends Component {
   constructor(props) {
     super(props);
     this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+    this.state = {
+      format: "hex",
+    };
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   gatherShades(palette, colorToFilterBy) {
@@ -21,19 +27,26 @@ class SingleColorPalette extends Component {
     return shades.slice(1);
   }
 
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
+
   render() {
+    const { format } = this.state;
+    const { paletteName, emoji } = this.props.palette;
     const colorBoxes = this._shades.map((color) => (
       <ColorBox
         key={color.id}
         name={color.name}
-        background={color.hex}
+        background={color[format]}
         showLink={false}
       />
     ));
     return (
       <div className="Palette">
-        <h1>Single Color Palette</h1>
+        <Navbar handleChange={this.changeFormat} showingAllColors={false} />
         <div className="Palette-colors">{colorBoxes}</div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
